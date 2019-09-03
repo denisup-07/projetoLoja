7a5b5825190a81778d24a19b86e6827e5b17c9cb
denisup-07 <denis.aguiar@dellead.com>
2019-09-03 13:11:23 -0300

    Mudança no login

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import {UserService} from './user/user.service';
+import {TokenService} from './token/token.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +10,7 @@ export class AuthService {
   urlAPI = 'http://localhost:8080/api/v1';
 
   constructor(private http: HttpClient,
-    private userService: UserService) { }
+    private userService: TokenService) { }
 
   authenticate(email: string, senha: string){
     return this.http.post<any>(this.urlAPI+'/login',{email,senha}).pipe(
